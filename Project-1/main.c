75280b8925f72dc68bde2398b14cfb583c60a95a
Luis Henrique <75594301+EngComp-Henrique@users.noreply.github.com>
2022-09-17 10:08:07 -0300

    Add files via upload

---
+++ Project-1/main.c
@@ -0,0 +1,44 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  Ac6
+  * @version V1.0
+  * @date    01-December-2013
+  * @brief   Default main function.
+  ******************************************************************************
+*/
+
+
+#include "stm32f4xx.h"
+
+void Delay(int delay)
+{
+	while (delay) --delay;
+}
+			
+
+int main(void)
+{
+
+	RCC->AHB1ENR |= 1;				// Liga o clock do GPIO
+	GPIOA->MODER |= (0b01 << 12);	// configura o pino PAC como saída
+
+	// GPIOA->ODR &= (1 << 6);
+	// GPIOA->ODR |= (1 << 6);
+
+	// while (1);
+
+	int delay_off = 50000000;
+	int delay_on = 100000;
+
+	while (1) {
+		GPIOA->ODR |= (1 << 6);		//Nível lógico alto no pino 6
+		Delay(delay_off);
+		GPIOA->ODR &= ~(1 << 6);	// Nível lógico baixo no pino 6
+		Delay(delay_on);
+		delay_off = delay_off / 2;
+		if (delay_off == 0) delay_off = 50000000;
+	}
+
+	return 1;
+}
